50c05b21cddab4eff0169b7950f24c0c1dabbe1b
dylan <150736124+dylandibeneditto@users.noreply.github.com>
2024-03-23 13:04:49 -0400

    feat: answer creater w/ or w/o duplicate numbers

--- code-breaker/js/index.js
+++ code-breaker/js/index.js
@@ -1,19 +1,14 @@
 import CodeBreaker from "./code-breaker.js";
+import generateProblem from "./problem-generator.js";
 
-const app = new CodeBreaker({
-    hints: [
-        [1,2,3,4],
-        [1,2,3,4],
-        [1,2,3,4],
-        [1,2,3,4],
-    ],
-    answer: '1234'
-})
+const app = new CodeBreaker(
+    generateProblem()
+)
 
 //  number button press events
 Array.from(document.getElementById('numbers').children).forEach((item, index) => {
     item.addEventListener('click', () => {
-        if(!app.terminal) {
+        if (!app.terminal) {
             app.input(index)
         }
     })
@@ -21,14 +16,14 @@ Array.from(document.getElementById('numbers').children).forEach((item, index) =>
 
 //  backspace press event
 document.getElementById('num-back').addEventListener('click', () => {
-    if(!app.terminal) {
+    if (!app.terminal) {
         app.backspace()
     }
 })
 
 //  enter press event
-document.getElementById('num-enter').addEventListener('click', ()=> {
-    if(!app.terminal) {
+document.getElementById('num-enter').addEventListener('click', () => {
+    if (!app.terminal) {
         app.check()
     }
 })
--- code-breaker/js/problem-generator.js
+++ code-breaker/js/problem-generator.js
@@ -1,5 +1,23 @@
-export default class ProblemGenerator {
-    constructor() {
+export default function generateProblem(duplicates = false) {
 
+    const getAnswer = () => {
+        let result = [];
+        for (let i = 0; i < 4; i++) {
+            let r = Math.round(Math.random() * 9);
+            if (!duplicates) {
+                while (result.includes(r)) {
+                    r = Math.round(Math.random() * 9);
+                }
+            }
+            result.push(r);
+        }
+        return result.join('');
+    }
+
+    return {
+        hints: [
+
+        ],
+        answer: getAnswer()
     }
 }
