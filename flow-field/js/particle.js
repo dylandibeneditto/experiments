90d4b28867c6ddd672621995c8cf0065fb35ffce
dylan <150736124+dylandibeneditto@users.noreply.github.com>
2024-03-24 21:47:42 -0400

    fix: particle not reading correct vector

--- flow-field/js/particle.js
+++ flow-field/js/particle.js
@@ -1,19 +1,20 @@
 export default class Particle {
-    constructor(x, y, positionFunc, s) {
-        this.x = x;
-        this.y = y;
-        this.start = positionFunc;
+    constructor(s) {
         this.s = s;
+        [this.x, this.y] = this.restart()
     }
 
     pos(vs) {
-        const xn = (Math.floor(this.x/this.s.w)/this.s.f)
-        const yn = (Math.floor(this.y/this.s.h)/this.s.f)
+        if(this.x<=0||this.y<=0||this.x>=this.s.w||this.y>=this.s.h) {
+            [this.x,this.y] = this.restart();
+        }
+        const xn = (Math.floor((this.x/this.s.w)*this.s.f))
+        const yn = (Math.floor((this.y/this.s.h)*this.s.f))
         this.x += Math.sin(vs[yn][xn]);
         this.y += Math.cos(vs[yn][xn]);
     }
 
-    destroy() {
-        [this.x,this.y] = this.start();
+    restart() {
+        return [(((Math.random()-.5)*this.s.w)+this.s.w/2),(((Math.random()-.5)*this.s.h)+this.s.h/2)];
     }
 }
