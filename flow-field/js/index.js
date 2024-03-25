4397f40df70e32205dbc14975afc2db4cd613c87
dylan <150736124+dylandibeneditto@users.noreply.github.com>
2024-03-24 21:47:56 -0400

    feat: interchangable number of particles

--- flow-field/js/index.js
+++ flow-field/js/index.js
@@ -4,9 +4,10 @@ let s = {
     w: window.innerWidth,
     h: window.innerHeight,
     //  frequency of vector map
-    f: 15
+    f: 100
 }
 
+let pcount = 100
 let a = 0;
 
 let vs = [];
@@ -18,31 +19,37 @@ canvas.height = s.h;
 
 const c = canvas.getContext('2d');
 
-const part = new Particle(400,400,1,s)
+let part = []
+for (let i = 0; i < pcount; i++) {
+    part.push(new Particle(s))
+}
 
 animate();
 
 function animate() {
-    
-    a+=.01
 
-    c.clearRect(0,0,s.w,s.h);
+    a += .01
 
-    for(let y = 0; y < s.f; y++) {
+    c.clearRect(0, 0, s.w, s.h);
+
+    for (let y = 0; y < s.f; y++) {
         vs.push([])
-        for(let x = 0; x < s.f; x++) {
+        for (let x = 0; x < s.f; x++) {
             vs[y][x] = a
             c.beginPath()
-            const px = x*(s.w/s.f)+((s.w/s.f)/2)
-            const py = y*(s.h/s.f-1)+((s.h/s.f)/2)
-            c.moveTo(px,py)
-            c.lineTo(px+Math.sin(vs[y][x])*((s.w/s.f)/2),py+Math.cos(vs[y][x])*((s.h/s.f)/2))
+            const px = x * (s.w / s.f) + ((s.w / s.f) / 2)
+            const py = y * (s.h / s.f) + ((s.h / s.f) / 2)
+            c.moveTo(px, py)
+            c.lineTo(px + Math.sin(vs[y][x]) * ((s.w / s.f) / 2), py + Math.cos(vs[y][x]) * ((s.h / s.f) / 2))
             c.stroke()
         }
     }
 
-    part.pos(vs)
-    c.fillRect(part.x, part.y,10,10)
+    part.forEach(item => {
+        item.pos(vs)
+        c.fillRect(item.x - 5, item.y - 5, 10, 10)
+    })
+
 
     requestAnimationFrame(animate)
 }
