7700e4513421a4ca6eb1b26aafede51d2ca590da
dylan <150736124+dylandibeneditto@users.noreply.github.com>
2024-03-25 11:20:22 -0400

    feat: display and input functionality as well as score function

--- tictactoe-minimax/js/index.js
+++ tictactoe-minimax/js/index.js
@@ -1,35 +1,55 @@
 const s = {
     player: 0,
     ai: 1,
-    lookup: ['','x','o']
+    lookup: ['', 'x', 'o']
 }
 
-const board = [
-    [0, 1, 0],
-    [0, 2, 0],
+let move = 0;
+
+let board = [
+    [0, 0, 0],
+    [0, 0, 0],
     [0, 0, 0],
 ]
 
+//  board display given board and id
 function display(b, id) {
     const el = document.getElementById(id);
     //  i could optimize this but i don't want to
     el.innerHTML = `
     <div class="trow">
-        <div id="t1,1">${s.lookup[b[0][0]]}</div>
-        <div id="t2,1">${s.lookup[b[0][1]]}</div>
-        <div id="t3,1">${s.lookup[b[0][2]]}</div>
+        <div id="00">${s.lookup[b[0][0]]}</div>
+        <div id="01">${s.lookup[b[0][1]]}</div>
+        <div id="02">${s.lookup[b[0][2]]}</div>
     </div>
     <div class="trow">
-        <div id="t1,2">${s.lookup[b[1][0]]}</div>
-        <div id="t2,2">${s.lookup[b[1][1]]}</div>
-        <div id="t3,2">${s.lookup[b[1][2]]}</div>
+        <div id="10">${s.lookup[b[1][0]]}</div>
+        <div id="11">${s.lookup[b[1][1]]}</div>
+        <div id="12">${s.lookup[b[1][2]]}</div>
     </div>
     <div class="trow">
-        <div id="t1,3">${s.lookup[b[2][0]]}</div>
-        <div id="t2,3">${s.lookup[b[2][1]]}</div>
-        <div id="t3,3">${s.lookup[b[2][2]]}</div>
+        <div id="20">${s.lookup[b[2][0]]}</div>
+        <div id="21">${s.lookup[b[2][1]]}</div>
+        <div id="22">${s.lookup[b[2][2]]}</div>
     </div>
     `
 }
 
+function alterDisplay(val, index, id) {
+    // i'll have to fix this later
+    document.getElementById(`${index.join('')}`).innerHTML = s.lookup[val]
+}
+
 display(board, 'playBoard')
+
+//  event listeners for click
+Array.from(document.querySelectorAll('#playBoard > * > *')).forEach(item => {
+    item.addEventListener('click', () => {
+        const coord = item.id.split('')
+        if (board[parseInt(coord[0])][parseInt(coord[1])] == 0 && move == s.player) {
+            board[parseInt(coord[0])][parseInt(coord[1])] = s.player + 1;
+            alterDisplay(s.player + 1, [parseInt(coord[0]), parseInt(coord[1])], 'playBoard')
+            move = 1 - move;
+        }
+    })
+})
---
+++ tictactoe-minimax/js/score.js
@@ -0,0 +1,13 @@
+/*
+ * score function for evaluating a position
+ * @param b - the board array
+ * @return - a value between -10 and 10
+ * 
+ * 10 - 1 is winning
+ * -10 - 2 is winning
+ * 0 - game will end in a tie
+ * 
+ */
+export default function score(b) {
+    
+}
