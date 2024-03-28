a6c3ba38774536075377de67b3ac1c0e09de0db9
Dylan DiBeneditto <dibenedittod@gmail.com>
2024-03-28 13:11:48 -0400

    new project: ascii generator

---
+++ ascii-generator/js/index.js
@@ -0,0 +1,25 @@
+const canvas = document.getElementById("canvas")
+
+const c = canvas.getContext('2d')
+
+const fileInput = document.querySelector('input[type="file"');
+
+fileInput.onchange = e => {
+  // just handling single file upload
+  const file = e.target.files[0];
+
+  const reader = new FileReader();
+  reader.onload = event => {
+    const image = new Image();
+    image.onload = () => {
+      canvas.width = image.width;
+      canvas.height = image.height;
+
+      c.drawImage(image, 0, 0);
+    };
+
+    image.src = event.target.result;
+  };
+
+  reader.readAsDataURL(file);
+};
